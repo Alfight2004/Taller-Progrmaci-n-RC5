7294efbbba76581718aa3536926e5307df27ad7e
Alejo Acosta <130623435+Alfight2004@users.noreply.github.com>
2025-05-25 20:38:47 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,1 @@
-public class Cliente extends Persona {
-    public Cliente(String nombre, String cedula) {
-        super(nombre, cedula);
-    }
 
-    @Override
-    public void mostrarInfo() {
-        System.out.println("Cliente registrado:");
-        super.mostrarInfo();
-    }
-}
